097adcba8e7e9123a2ba4d6f1f077f6a4305095c
Hellboyinc <55628165+Hellboyinc@users.noreply.github.com>
2019-11-26 17:26:10 +0200

    no message

--- js/script.js
+++ js/script.js
@@ -14,3 +14,67 @@ var template = {
 }
 
 template.init();
+
+ $(document).ready(function() {
+    $('section.main .slides').lightSlider({
+        gallery:false,
+        item:1,
+        thumbItem:9,
+        slideMargin: 0,
+        speed:500,
+        auto:false,
+        loop:true,
+        onSliderLoad: function() {
+            $('section.main .slides').removeClass('cS-hidden');
+        }  
+    });
+
+    if ( (window.matchMedia("(max-width: 1920px)").matches) && (window.matchMedia("(min-width: 1025px)").matches) ) {
+	    $('section.projects .slides').lightSlider({
+	        gallery:false,
+	        item:3,
+	        thumbItem:1,
+	        slideMargin: 0,
+	        speed:500,
+	        auto:false,
+	        loop:true,
+	        onSliderLoad: function() {
+	            $('section.main .slides').removeClass('cS-hidden');
+	        }
+	    });
+	} else 
+
+    if ((window.matchMedia("(max-width: 1024px)").matches) && (window.matchMedia("(min-width: 769px)").matches) ) {
+    	$('section.projects .slides').lightSlider({
+	        gallery:false,
+	        item:2,
+	        thumbItem:1,
+	        slideMargin: 0,
+	        speed:500,
+	        auto:false,
+	        loop:true,
+	        adaptiveHeight:true,
+	        onSliderLoad: function() {
+	            $('section.main .slides').removeClass('cS-hidden');
+	        }
+	    });
+    } else 
+
+	if (window.matchMedia("(max-width: 768px)").matches) {
+    	$('section.projects .slides').lightSlider({
+	        gallery:false,
+	        item:1,
+	        thumbItem:1,
+	        slideMargin: 0,
+	        speed:500,
+	        auto:false,
+	        loop:true,
+	        adaptiveHeight:true,
+	        onSliderLoad: function() {
+	            $('section.main .slides').removeClass('cS-hidden');
+	        }
+	    });
+    }
+
+});
+
